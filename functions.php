8e8c6f17a7ce4c4dd3f42490ca429180c8cedada
jomon <jmjonly4you@gmail.com>
2015-05-14 08:01:45 +0530

    working on nav menu

--- functions.php
+++ functions.php
@@ -9,6 +9,13 @@
 add_theme_support('post-thumbnails');
 // Adding html5 search form
 add_theme_support('html5', array('search-form'));
+
+//  Register custom navigation menu
+function register_nakshathra_menu() {
+    register_nav_menu('nakshathra-header-menu',__( 'Nakshathra Header Menu' ));
+}
+add_action( 'init', 'register_nakshathra_menu' );
+
 function hotProperties()
 {
     $args = array('numberposts' => 10,  /* get 4 posts, or set -1 for all */
@@ -84,3 +91,57 @@ function jo_search_form($form)
 }
 
 add_filter('get_search_form', 'jo_search_form');
+
+
+function joCustomMenuItems(){
+    // Get the nav menu based on $menu_name (same as 'theme_location' or 'menu' arg to wp_nav_menu)
+    // This code based on wp_nav_menu's code to get Menu ID from menu slug
+    /*
+                                            $menu_name = 'nakshathra-header-menu';
+
+                                            if (($locations = get_nav_menu_locations()) && isset($locations[$menu_name])) {
+                                                $menu = wp_get_nav_menu_object($locations[$menu_name]);
+                                                $menu_items = wp_get_nav_menu_items($menu->term_id);
+
+                                                print_r($menu_items);
+                                                $menu_list = '<ul id="menu-' . $menu_name . '" class=" nav navbar-nav navbar-right">';
+                                                foreach ($menu_items as $menu_item) {
+                                                    /*
+                                                     [post_title] => Home
+                                                        [post_excerpt] =>
+                                                        [post_status] => publish
+                                                        [comment_status] => open
+                                                        [ping_status] => open
+                                                        [post_password] =>
+                                                        [post_name] => home
+                                                        [to_ping] =>
+                                                        [pinged] =>
+                                                        [post_modified] => 2015-05-14 01:56:33
+                                                        [post_modified_gmt] => 2015-05-14 01:56:33
+                                                        [post_content_filtered] =>
+                                                        [post_parent] => 0
+                                                        [guid] => http://127.0.0.1/wp/?p=39
+                                                        [url] => http://127.0.0.1/wp/
+
+    //                                                print_r($menu_item);
+    //                                                wp_die('');
+                                                    if ($menu_item->post_status == 'open') {
+                                                        $menu_list .= '<li><a href="' . $menu_item->url . '">' . $menu_item->post_title . '</a></li>';
+                                                    }
+                                                }
+                                                $menu_list .= '</ul>';
+                                            } else {
+                                                $menu_list = '<ul><li>Menu "' . $menu_name . '" not defined.</li></ul>';
+                                            }
+                                            */
+    // $menu_list now ready to output
+    /*
+    wp_nav_menu(array(
+        'theme_location' => 'nakshathra-header-menu',
+
+        "container" => 'ul',
+        'container_class' => 'nav navbar-nav navbar-right'
+
+    ));
+    */
+}
--- header.php
+++ header.php
@@ -6,69 +6,85 @@ ini_set('error_reporting', E_ALL);
 <!DOCTYPE HTML>
 <html>
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<title>Welcome to Nakshathra Real  Estate  agency</title>
-<link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
-<link href='http://fonts.googleapis.com/css?family=Roboto:400,100,100italic,300,300italic,400italic,500,500italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Welcome to Nakshathra Real Estate agency</title>
+    <link
+        href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800'
+        rel='stylesheet' type='text/css'>
+    <link
+        href='http://fonts.googleapis.com/css?family=Roboto:400,100,100italic,300,300italic,400italic,500,500italic,700,700italic,900,900italic'
+        rel='stylesheet' type='text/css'>
 
-<!-- Latest compiled and minified CSS -->
-<link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/css/bootstrap.css">
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/css/bootstrap.css">
 
-<!-- Optional theme -->
-<link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/css/bootstrap-theme.css">
-<script src="<?php bloginfo('template_url'); ?>/js/jquery-1.10.2.js"></script>
-<script src="<?php bloginfo('template_url'); ?>/js/bootstrap.js"></script>
-<link href="<?php bloginfo('template_url'); ?>/css/nakshathra_style.css" rel="stylesheet" type="text/css">
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/css/bootstrap-theme.css">
+    <script src="<?php bloginfo('template_url'); ?>/js/jquery-1.10.2.js"></script>
+    <script src="<?php bloginfo('template_url'); ?>/js/bootstrap.js"></script>
+    <link href="<?php bloginfo('template_url'); ?>/css/nakshathra_style.css" rel="stylesheet" type="text/css">
 </head>
 
 <body>
 <div class="main_bg"></div>
 <div class="container">
-<div class="row">
-  <div class="col-md-12">
-    <div class="col-md-5"><img src="<?php bloginfo('template_url'); ?>/images/nakshathra_logo.png" class="img-responsive logo_p"></div>
-    <div class="col-md-7">
-      <div class="top_menu_bk">
-        <div class="row">
-          <div class="hm_contact"> <span class="ph_top">+91 924 941 7377</span><br/>
-            <span class="mail_txt">info@nakshathra.com</span> </div>
-        </div>
-        <div class="row">
-          <div class="menu_top">
-            <nav class="navbar navbar-default" role="navigation">
-              <div class="container-fluid"> 
-                <!-- Brand and toggle get grouped for better mobile display -->
-                <div class="navbar-header">
-                  <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1"> <span class="sr-only">Toggle navigation</span> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button>
+    <div class="row">
+        <div class="col-md-12">
+            <div class="col-md-5"><img src="<?php bloginfo('template_url'); ?>/images/nakshathra_logo.png"
+                                       class="img-responsive logo_p"></div>
+            <div class="col-md-7">
+                <div class="top_menu_bk">
+                    <div class="row">
+                        <div class="hm_contact"><span class="ph_top">+91 924 941 7377</span><br/>
+                            <span class="mail_txt">info@nakshathra.com</span></div>
+                    </div>
+                    <div class="row">
+                        <div class="menu_top">
+                            <nav class="navbar navbar-default" role="navigation">
+                                <div class="container-fluid">
+                                    <!-- Brand and toggle get grouped for better mobile display -->
+                                    <div class="navbar-header">
+                                        <button type="button" class="navbar-toggle" data-toggle="collapse"
+                                                data-target="#bs-example-navbar-collapse-1"><span class="sr-only">Toggle navigation</span>
+                                            <span class="icon-bar"></span> <span class="icon-bar"></span> <span
+                                                class="icon-bar"></span></button>
+                                    </div>
+
+                                    <!-- Collect the nav links, forms, and other content for toggling -->
+                                    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+
+                                        <?php
+
+                                        wp_nav_menu( array(
+                                            'theme_location'    => 'nakshathra-header-menu',
+                                            'container'         => 'ul',
+                                            'container_class'   => 'nav navbar-nav navbar-right',
+                                            'menu_id'           => false,
+                                            'sort_column'       => 'menu_order',
+                                            'depth'             => '4',
+                                            'fallback_cb'     => 'kstWpNavMenuFallbackCb'
+                                        ) );
+
+//                                        echo '</pre>';
+                                        ?>
+
+                                        <ul class="nav navbar-nav navbar-right">
+                                            <li class="active"><a href="#">HOME</a></li>
+                                            <li><a href="#">ABOUT US</a></li>
+                                            <li><a href="#">LAND </a></li>
+                                            <li><a href="#">PROPERTIES</a></li>
+                                            <li><a href="#">CONTACT US</a></li>
+                                        </ul>
+                                    </div>
+                                    <!-- /.navbar-collapse -->
+                                </div>
+                                <!-- /.container-fluid -->
+
+                            </nav>
+                        </div>
+                    </div>
                 </div>
-                
-                <!-- Collect the nav links, forms, and other content for toggling -->
-                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-            
-             
-                  <?php wp_nav_menu( array(
-				   'theme_location' => 'primary',
-				 
-				   "container" => ''
-				   
-					 ) ); ?>
-                  <ul class="nav navbar-nav navbar-right">
-                    <li class="active"><a href="#">HOME</a></li>
-                    <li><a href="#">ABOUT US</a></li>
-                    <li><a href="#" >LAND </a></li>
-                    <li><a href="#" >PROPERTIES</a></li>
-                    <li><a href="#">CONTACT US</a></li>
-                  </ul>
-                </div>
-                <!-- /.navbar-collapse --> 
-              </div>
-              <!-- /.container-fluid --> 
-              
-            </nav>
-          </div>
+            </div>
         </div>
-      </div>
     </div>
-  </div>
-</div>
